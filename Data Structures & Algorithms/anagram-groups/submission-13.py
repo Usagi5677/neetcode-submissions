fc5497d20820a8872c1c3814811d8b8338ef2458
Naish <65016084+Usagi5677@users.noreply.github.com>
2026-05-13 17:35:55 +0900

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs:
+            signature = [0] * 26
+            for c in s:
+                signature[ord(c) - ord("a")] += 1
+            res[tuple(signature)].append(s)
+
+        return list(res.values())
