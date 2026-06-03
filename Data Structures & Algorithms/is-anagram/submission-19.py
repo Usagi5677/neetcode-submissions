696fadd4af1a439d8d34d1f5a5a979508479b895
Naish <65016084+Usagi5677@users.noreply.github.com>
2026-06-03 23:21:57 +0900

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hm, hm2 = {}, {}
+        for i in range(len(s)):
+            hm[s[i]] = hm.get(s[i],0) + 1
+            hm2[t[i]] = hm2.get(t[i],0) + 1
+        
+        return hm == hm2
