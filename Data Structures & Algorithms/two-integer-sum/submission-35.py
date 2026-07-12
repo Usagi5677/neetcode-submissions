c48dd26d4e95daf5c6befb3220bd6b3d65d784cf
Naish <65016084+Usagi5677@users.noreply.github.com>
2026-07-13 03:55:03 +0900

    Add: two-integer-sum - submission-35

---
+++ Data Structures & Algorithms/two-integer-sum/submission-35.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+
+        for i, n in enumerate(nums):
+            temp = target - n
+            if temp in hm:
+                return [hm.get(temp), i]
+            hm[n] = i
+        
+        
+        
+        
+        
